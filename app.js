74a4a38f90c7edefb839ea9d99a3378fedff90a3
unknown <nilesh.n.isc@gmail.com>
2024-09-23 15:08:58 +0530

    Change message 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to K21 Academy learning!'));
+app.get('/', (req, res) => res.send('Welcome to K21 Academy projects'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
